b23b06e700950f8ee1165e84fb7609a2cbcb853d
Tony G. Bolaño <tonybolanyo@gmail.com>
2017-09-11 22:33:47 +0200

    Probando EJS

--- nodeapi/routes/index.js
+++ nodeapi/routes/index.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  const segundo = new Date().getSeconds();
+
+  res.render('index', {
+    title: 'Express',
+    valor: '<script>alert("envía 1 bitcoin a ... para limpiar tu navegador");</script>',
+    condicion: {
+      segundo: segundo,
+      estado: segundo % 2 === 0
+    },
+    users: [
+      { name: 'Jones', age: 29},
+      { name: 'Smith', age: 48},
+      { name: 'Brown', age: 33}
+    ]
+  });
 });
 
 module.exports = router;
